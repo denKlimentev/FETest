c22bd8198f7875908c52d66ecab829cb2f10e6bc
d.klimentev <serk19485>
2022-01-16 16:55:12 +0300

    add exception add flow with random product add calculation with tax

--- support/helpers.js
+++ support/helpers.js
@@ -4,18 +4,24 @@ let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 exports.click = (element) => {
+    if (element === undefined) {
+        throw new TypeError('element is not provide');
+    }
     element.waitForClickable();
     element.click();
 };
 
 exports.setValue = (element, value) => {
+    if (element === undefined || value === undefined) {
+        throw new TypeError('element and value is not provide');
+    }
     element.waitForClickable();
     element.setValue(value);
 };
 
-
-exports.calculation = (numberOne, numberTwo) => {
-    const one = +numberOne.toString().replace('$', '');
-    const two = +numberTwo.toString().replace('$', '');
-    return `$${one + two}`
-};
+exports.findElementInParent = (parent, element) => {
+    if (parent === undefined || element === undefined) {
+        throw new TypeError('parent and element is not provide');
+    }
+    return parent.$(element.selector)
+}
--- test/saucedemoPortal/purchaseFlowTest.js
+++ test/saucedemoPortal/purchaseFlowTest.js
@@ -5,54 +5,54 @@ const userObject = require('../../logic/userObject.js');
 const testAccts = require('../../testData/testAccounts.json');
 const pages = require('../../testData/pages/PageFactory.js');
 
-const productName = 'Sauce Labs Backpack';
-const tax = 2.40;
+const tax = 0.08;
 describe('Automate the purchase flow ', () => {
-    let user;
+    let user, productName ;
 
     beforeEach('Precondition : login', () => {
         user = new userObject();
         user.login(testAccts.standardUser, testAccts.password)
+        productName = user.getRandomProductName();
+        console.log(productName);
     });
 
     it('Purchase flow', () => {
         user.addProductToCart(productName);
-        const itemPrice = pages.ProductListPage.getProductDescriptionInTheList(productName)
-            .$(pages.ProductListPage.itemPrice_div)
-            .getText();
+        const itemPrice = user.getPrise(productName)
+        const itemTax = parseFloat(itemPrice * tax).toFixed(2);
 
         assert.softAssert(
-            pages.ProductListPage.shopping_cart_link.$(pages.ProductListPage.span).getText()
+            h.findElementInParent(pages.ProductListPage.shopping_cart_link, pages.ProductListPage.span).getText()
             , '1'
             , 'Number of purchases must be one'
         );
         assert.softTrue(
-            pages.ProductListPage.getProductDescriptionInTheList(productName).$(pages.ProductListPage.remove_button).isDisplayed()
+            h.findElementInParent(pages.ProductListPage.getProductDescriptionInTheList(productName),pages.ProductListPage.remove_button).isDisplayed()
             , 'Button removed in added product must be visible'
         );
 
         user.goToCartPage();
         assert.softAssert(pages.CartPage.itemName_div.getText(), productName);
-        assert.softContains(pages.CartPage.itemPrice_div.getText(), itemPrice);
+        assert.softContains(pages.CartPage.itemPrice_div.getText(), `$${itemPrice}`);
 
         user.clickCheckoutGotoStepOne();
         user.putRandomDataAndCompleteStepOne();
 
         assert.softAssert(pages.StepTwoPage.itemName_div.getText(), productName);
-        assert.softAssert(pages.StepTwoPage.itemPrice_div.getText(), itemPrice);
-           assert.softContains(
-               pages.StepTwoPage.summaryInfo_box.$(pages.StepTwoPage.subtotal_label).getText()
-            , itemPrice
+        assert.softAssert(pages.StepTwoPage.itemPrice_div.getText(), `$${itemPrice}`);
+        assert.softContains(
+            h.findElementInParent(pages.StepTwoPage.summaryInfo_box, pages.StepTwoPage.subtotal_label).getText()
+            , `$${itemPrice}`
             , 'Subtotal must be equel item price'
         );
         assert.softContains(
-            pages.StepTwoPage.summaryInfo_box.$(pages.StepTwoPage.tax_label).getText()
-            , `$${tax}`
+            h.findElementInParent(pages.StepTwoPage.summaryInfo_box, pages.StepTwoPage.tax_label).getText()
+            , `$${itemTax}`
             , 'Tax must be equel defalte tax'
         );
         assert.softContains(
-            pages.StepTwoPage.summaryInfo_box.$(pages.StepTwoPage.total_label).getText()
-            , `${h.calculation(tax, itemPrice)}`
+            h.findElementInParent(pages.StepTwoPage.summaryInfo_box, pages.StepTwoPage.total_label).getText()
+            , `$${parseFloat(+itemPrice + +itemTax).toFixed(2)}`
             , 'total must be equel defalte tax + item price'
         );
 
